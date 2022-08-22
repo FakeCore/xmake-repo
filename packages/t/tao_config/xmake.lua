e8b4a32520c7cdb1b7acdea6d1eb92ec0f236639
FakeCore <flowunlight@gmail.com>
2022-08-22 14:43:55 +0800

    update repository:tao_config v1.1

--- packages/t/tao_config/xmake.lua
+++ packages/t/tao_config/xmake.lua
@@ -2,10 +2,11 @@ package("tao_config")
     set_homepage("https://github.com/FakeCore/config")
     set_description("C++ header-only config reader library based on JSON")
 
-    set_urls("https://github.com/FakeCore/config/releases/download/v1.0/tao_config.$(version).tar.gz",{alias="github"})
+    set_urls("https://github.com/FakeCore/config/releases/download/$(version)/tao_config.$(version).tar.gz",{alias="github"})
 
 
-    add_versions("github:v1.0","69ecff2a98da1040f38dfe785813b6e6a4a1e1cbc205c4524dc15527eee362b1")
+    add_versions("github:v1.1","fb1e1b7bb415a852a35939c72c8f8e2bbdd82d0b93c9b3ad44a38042a6178442")
+
 
     add_deps("cmake")
     on_install("windows", "macosx", "linux", function (package)
